7ce5fabe8a4158120921ba3fe615bea7afd20bda
UbuntuJax <jacksonbledsoe@gmail.com>
2022-06-30 14:56:23 +1000

    Initial commit

---
+++ hsprob.m
@@ -0,0 +1,27 @@
+function probability = hsprob(deckSize, turnNo, cardNo, testNo, coin)
+    
+    %HSPROB  Computes liklihood of drawing a desired card given arbitrary
+    %input arguments, currently in prototype phase
+    
+    probability = 1;
+    varType = class(deckSize);
+    fprintf("%s\n", varType);
+    
+    %check args are all present
+    
+    
+    %check that args are of type double
+    
+    %apply relevant arg values
+    
+    %perform computation
+    %Mulligan
+    mulliganDoubleArray = double.empty(4,0);
+    disp(mulliganDoubleArray);
+    
+    %Reset Mulligan
+    %Draw Probability
+    
+    
+    
+end
